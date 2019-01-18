48668c7e2168013ca3a3810992a4ba70c2f62d17
TimurBad <42368409+TimurBad@users.noreply.github.com>
2019-01-18 23:12:20 +0400

    доп изменения

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -1,12 +1,27 @@
 package ru.stqa.pft.sanbox;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-public static void main(String[] args){
-System.out.println("Hello, world!");
-area();
-}
-public static void area(){
-    System.out.println("text");
-}
+    public static void main(String[] args) {
+        Square s = new Square(2);
+        Rectangle r = new Rectangle();
+        r.a = 4;
+        r.b = 6;
+        System.out.println("Hello, world!");
+
+        System.out.println("test " + s.l);
+        System.out.println("test " + area(s));
+        System.out.println("test " + area(r));
+
+    }
+
+    public static double area(Square s) {
+        return s.l * s.l;
+    }
+
+    public static double area(Rectangle r) {
+        return r.a * r.b;
+    }
+
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/Rectangle.java
@@ -0,0 +1,6 @@
+package ru.stqa.pft.sanbox;
+
+public class Rectangle {
+    public double a;
+    public double b;
+}
