3b2a3fb53da74d91aa74b5facdb91142b1db24e4
TimurBad <42368409+TimurBad@users.noreply.github.com>
2019-01-22 00:35:36 +0700

    все в кучу,жаль(

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/Test1.java
@@ -0,0 +1,83 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test1 {
+  public ChromeDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    String key= "webdriver.chrome.driver";
+    String value= "C:\\way_QA_Automation_Engineer\\webDriver\\chromedriver.exe";
+    System.setProperty(key,value);
+    driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test1() throws Exception {
+    driver.get("https://www.postable.com/login#/");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Get Addresses'])[1]/following::a[1]")).click();
+    driver.findElement(By.id("InputEmail1")).click();
+    driver.findElement(By.id("InputEmail1")).clear();
+    driver.findElement(By.id("InputEmail1")).sendKeys("bad@mail.ru");
+    driver.findElement(By.name("password")).click();
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("nhjkjkj19993");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Login'])[3]/following::button[1]")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -3,25 +3,16 @@ package ru.stqa.pft.sanbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        Square s = new Square(2);
-        Rectangle r = new Rectangle();
-        r.a = 4;
-        r.b = 6;
+        Square s = new Square(5);
+        Rectangle r = new Rectangle(4,6);
         System.out.println("Hello, world!");
 
-        System.out.println("test " + s.l);
-        System.out.println("test " + area(s));
-        System.out.println("test " + area(r));
+        System.out.println("test " + s.l + " = " + s.area());
+        System.out.println("test Rectangle =" + r.area());
 
     }
 
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
 
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
 
 
 }
